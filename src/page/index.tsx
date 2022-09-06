ad60f1574d8c4502d0e1ca392d53b96682d45619
mys1024 <mys.1024@outlook.com>
2022-09-07 02:38:10 +0800

    refactor: optimize

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,15 +1,13 @@
 import Logo from '~/component/Logo'
 import Intro from '~/component/Intro'
 
-export default () => {
-  return (
-    <div
-      space-y-12
-      flex flex-col
-      items-center
-    >
-      <Logo />
-      <Intro />
-    </div>
-  )
-}
+export default () => (
+  <div
+    space-y-12
+    flex flex-col
+    items-center
+  >
+    <Logo />
+    <Intro />
+  </div>
+)
